896d2357c984c85e15fb1bc3703a7fb038368a4a
mykhalchuk.m <mykhalchuk.m@48d5a77d-33a9-236d-e4a3-70fd8997a855>
2012-03-29 14:39:54 +0000

    delete old code

--- trunk/grizzly-base/src/com/grezlee/game/base/GameStatus.java
+++
@@ -1,5 +0,0 @@
-package com.grezlee.game.base;
-
-public enum GameStatus {
-	STRTED, PAUSED, STPPED;
-}
--- trunk/grizzly-tenis/src/com/grizzly/tenis/TenisGame.java
+++
@@ -1,7 +0,0 @@
-package com.grizzly.tenis;
-
-import com.grezlee.game.base.Game;
-
-public class TenisGame extends Game {
-
-}
