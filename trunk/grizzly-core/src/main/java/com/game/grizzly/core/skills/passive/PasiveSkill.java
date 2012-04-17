d19ceada0b6e6eb01b2dd81edb6c207bbf8cb806
mykhalchuk.m <mykhalchuk.m@48d5a77d-33a9-236d-e4a3-70fd8997a855>
2012-04-17 07:51:26 +0000

    removed old code

--- trunk/grizzly-core/src/main/java/com/game/grizzly/core/skills/passive/PasiveSkill.java
+++
@@ -1,7 +0,0 @@
-package com.game.grizzly.core.skills.passive;
-
-import com.game.grizzly.core.skills.Skill;
-
-public class PasiveSkill extends Skill {
-
-}
--- trunk/grizzly-core/src/main/java/com/game/grizzly/core/skills/passive/Stamian.java
+++
@@ -1,5 +0,0 @@
-package com.game.grizzly.core.skills.passive;
-
-public class Stamian extends PasiveSkill {
-
-}
