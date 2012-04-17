2e529c539374d07e56376db88db4b5b78638f2c6
mykhalchuk.m <mykhalchuk.m@48d5a77d-33a9-236d-e4a3-70fd8997a855>
2012-04-17 07:50:40 +0000

    removed old code

--- trunk/grizzly-core/src/main/java/com/game/grizzly/core/skills/defence/CurvedDefence.java
+++
@@ -1,5 +0,0 @@
-package com.game.grizzly.core.skills.defence;
-
-public class CurvedDefence extends Defence {
-
-}
--- trunk/grizzly-core/src/main/java/com/game/grizzly/core/skills/defence/Defence.java
+++
@@ -1,7 +0,0 @@
-package com.game.grizzly.core.skills.defence;
-
-import com.game.grizzly.core.skills.Skill;
-
-public class Defence extends Skill {
-
-}
